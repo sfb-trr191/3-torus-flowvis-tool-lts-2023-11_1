d8491f65ca953dd408a1a1f7fbf66451dd97cb16
Christian Lang <christian.lang92@gmx.de>
2021-07-10 22:08:16 +0200

    advanced settings axes

--- hide_manager.js
+++ hide_manager.js
@@ -43,6 +43,11 @@ class HideManager{
 
         this.group_settings = new HideGroup("select_settings_mode");
         this.group_settings.Add("input_row_scalar_field_debug", LEVEL_DEBUG);
+        this.group_settings.Add("input_row_bounding_axes_length", LEVEL_ADVANCED);
+        this.group_settings.Add("input_row_bounding_axes_radius", LEVEL_ADVANCED);
+        this.group_settings.Add("input_row_emphasize_origin_axes", LEVEL_ADVANCED);
+        this.group_settings.Add("input_row_bounding_axes_origin_length", LEVEL_ADVANCED);
+        this.group_settings.Add("input_row_bounding_axes_origin_radius", LEVEL_ADVANCED);
         this.groups.push(this.group_settings);
     }
 
