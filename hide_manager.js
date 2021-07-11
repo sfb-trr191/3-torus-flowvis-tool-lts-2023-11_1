f3f0adb40d90d9319244a67ce85c54d6f344dc06
Christian Lang <christian.lang92@gmx.de>
2021-07-11 20:42:24 +0200

    hide 2nd view, css improvements

--- hide_manager.js
+++ hide_manager.js
@@ -2,6 +2,8 @@ const LEVEL_BASIC = 0;
 const LEVEL_ADVANCED = 1;
 const LEVEL_DEBUG = 2;
 
+const CLASS_INPUT_ROW = "input_row";
+
 class InputRow{
     constructor(name, required_level){
         this.name = name;
@@ -15,9 +17,23 @@ class InputRow{
     }
 }
 
+class Show{
+    constructor(name, required_level){
+        this.name = name;
+        this.required_level = required_level;
+        this.element = document.getElementById(name)
+    }
+
+    UpdateVisibility(level){
+        var visible = level >= this.required_level;
+        this.element.className = visible ? "show" : "hidden";
+    }
+}
+
 class HideGroup{
     constructor(select_name){
         this.list_input_row = [];
+        this.list_show = [];
         this.select = document.getElementById(select_name);
         this.select.addEventListener("change", (event) => {
             this.UpdateVisibility();
@@ -30,10 +46,17 @@ class HideGroup{
         for(var i=0; i<this.list_input_row.length; i++){
             this.list_input_row[i].UpdateVisibility(level);
         }
+        for(var i=0; i<this.list_show.length; i++){
+            this.list_show[i].UpdateVisibility(level);
+        }
     }
 
-    Add(input_row_name, level){
+    AddInputRow(input_row_name, level){
         this.list_input_row.push(new InputRow(input_row_name, level));
+    }
+
+    AddShow(show_name, level){
+        this.list_show.push(new Show(show_name, level));
     }
 }
 
@@ -42,13 +65,17 @@ class HideManager{
         this.groups = [];
 
         this.group_settings = new HideGroup("select_settings_mode");
-        this.group_settings.Add("input_row_scalar_field_debug", LEVEL_DEBUG);
-        this.group_settings.Add("input_row_bounding_axes_length", LEVEL_ADVANCED);
-        this.group_settings.Add("input_row_bounding_axes_radius", LEVEL_ADVANCED);
-        this.group_settings.Add("input_row_emphasize_origin_axes", LEVEL_ADVANCED);
-        this.group_settings.Add("input_row_bounding_axes_origin_length", LEVEL_ADVANCED);
-        this.group_settings.Add("input_row_bounding_axes_origin_radius", LEVEL_ADVANCED);
+        this.group_settings.AddInputRow("input_row_scalar_field_debug", LEVEL_DEBUG);
+        this.group_settings.AddInputRow("input_row_bounding_axes_length", LEVEL_ADVANCED);
+        this.group_settings.AddInputRow("input_row_bounding_axes_radius", LEVEL_ADVANCED);
+        this.group_settings.AddInputRow("input_row_emphasize_origin_axes", LEVEL_ADVANCED);
+        this.group_settings.AddInputRow("input_row_bounding_axes_origin_length", LEVEL_ADVANCED);
+        this.group_settings.AddInputRow("input_row_bounding_axes_origin_radius", LEVEL_ADVANCED);
         this.groups.push(this.group_settings);
+
+        this.group_side_canvas = new HideGroup("select_side_mode");
+        this.group_side_canvas.AddShow("show_side_canvas", 1);
+        this.groups.push(this.group_side_canvas);
     }
 
     UpdateVisibility(){
