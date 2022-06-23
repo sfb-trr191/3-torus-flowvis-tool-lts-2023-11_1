0e5fcf87a24aa90d2b2d0f49cf954cf4af72959b
Christian Lang <christian.lang92@gmx.de>
2022-06-23 17:07:45 +0200

    VERSION 2022-06.1: "new ui release", redirect all old versions to 2022-03.1

--- version_redirection_dict.js
+++ version_redirection_dict.js
@@ -1,3 +1,19 @@
 exports.VERSION_REDIRECTION_DICT = {
-    "1337-01.1" : "https://christian-lang-git.github.io/reeb-vector-fields-web/index.html" //test entry, redirects to development version
+    "1337-01.1" : "https://christian-lang-git.github.io/reeb-vector-fields-web/index.html", //test entry, redirects to development version
+    "2021-12.1" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2021-12.2" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2021-12.3" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2021-12.4" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-01.1" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-01.2" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-01.3" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-02.1" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-02.2" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-02.3" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-02.4" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-02.5" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-02.6" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-02.7" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+    "2022-03.1" : "https://sfb-trr191.github.io/3-torus-flowvis-tool-2022-03_1/index.html", //versions before ui overhaul
+
 }
