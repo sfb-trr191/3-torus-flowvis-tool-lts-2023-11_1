47203d288c34c6fc59d269f64bf66029208d6fe8
Christian Lang <christian.lang92@gmx.de>
2023-11-19 23:35:43 +0100

    flow map now calculates relative values, start is added when using finite differences --> solves problem at border

--- shader/stand_alone/f_shader_compute_flow_map_slice.glsl
+++ shader/stand_alone/f_shader_compute_flow_map_slice.glsl
@@ -31,6 +31,7 @@ vec3 f(vec3 vector);
 int CountBorderDimensions(int x_index_extended, int y_index_extended, int z_index_extended);
 vec3 GetStartPosition(int count, int x_index_extended, int y_index_extended, int z_index_extended);
 int CalculateFinalPositionOld(vec3 start_position, inout vec3 final_position);
+int CalculateFinalPositionRelative(vec3 start_position, inout vec3 final_position);
 
 const float PI = 3.1415926535897932384626433832795;
 
@@ -83,8 +84,8 @@ void main()
     }
 
     vec3 start_position = GetStartPosition(count, x_index_extended, y_index_extended, z_index_extended);
-    vec3 final_position;
-    int iteration_count = CalculateFinalPositionOld(start_position, final_position);
+    vec3 final_position = vec3(0,0,0);
+    int iteration_count = CalculateFinalPositionRelative(start_position, final_position);
 
     /*
     if(int(gl_FragCoord[0]) % mod_num == 0){
@@ -200,6 +201,67 @@ vec3 GetStartPosition(int count, int x_index_extended, int y_index_extended, int
         start_position = phi(border_position, vector_from_border_to_this);
     }
     return start_position;
+}
+
+int CalculateFinalPositionRelative(vec3 start_position, inout vec3 final_position)    
+{
+    vec3 previous_position = start_position;
+    vec3 pos_r3 = final_position;//this is the vector that tracks the flow
+
+    vec3 previous_f = f(previous_position);
+    float previous_speed = length(previous_f);
+    float previous_cost = 0.0;//cost = time or length depending on termination_condition
+
+    vec3 current_position;
+    vec3 current_f;
+    float current_speed = 0.0;
+    float current_cost = 0.0;//cost = time or length depending on termination_condition
+
+    float segment_length = 0.0;
+    float average_speed = 0.0;
+    float arc_length = 0.0;
+    int iteration_count = 0;
+    for (int i=0; i<max_iterations; i++){
+        vec3 k1 = step_size * f(previous_position);
+		vec3 k2 = step_size * f(phi(previous_position, k1/2.0));
+		vec3 k3 = step_size * f(phi(previous_position, k2/2.0));
+		vec3 k4 = step_size * f(phi(previous_position, k3));
+				
+        vec3 dir = k1 / 6.0 + k2 / 3.0 + k3 / 3.0 + k4 / 6.0;
+		current_position = previous_position + dir;//this is the new position if we do NOT check out of bounds
+        vec3 difference = current_position - previous_position;//used later but needs to be calculated before applying rules
+        current_position = phi_track(previous_position, dir, pos_r3);//this is the new position if we DO check out of bounds
+
+        current_f = f(current_position);
+        current_speed = length(current_f);
+        segment_length = length(difference);
+        arc_length += segment_length;
+        average_speed = (previous_speed + current_speed) * 0.5;
+
+
+        //cost = time or length depending on termination_condition
+        if(termination_condition == FTLE_TERMINATION_CONDITION_ADVECTION_TIME){
+            current_cost = previous_cost + (segment_length / average_speed);
+            //stop if speed is below threshold or advection time is reached
+            if(current_cost > advection_time)
+                break;
+        }else{
+            //FTLE_TERMINATION_CONDITION_ARC_LENGTH
+            current_cost = arc_length;
+            //stop if speed is below threshold or advection time is reached
+            if(current_cost > termination_arc_length)
+                break;
+        }
+
+        //prepare next iteration
+        previous_position = current_position;
+        previous_f = current_f;
+        previous_cost = current_cost;
+        previous_speed = current_speed;
+        iteration_count = i;
+    }
+    final_position = pos_r3;
+    return iteration_count;
 }
 
 int CalculateFinalPositionOld(vec3 start_position, inout vec3 final_position)    
--- shader/stand_alone/f_shader_compute_flowmap_finite_differences.glsl
+++ shader/stand_alone/f_shader_compute_flowmap_finite_differences.glsl
@@ -18,10 +18,24 @@ out vec4 outputColor;
 
 vec3 f(vec3 vector);
 
+vec3 CalculateCentralDifference(int direction, float h2);
+vec3 CorrectionTerm(vec3 value_sample, vec3 position_sample);
+
 const float PI = 3.1415926535897932384626433832795;
-//! [0]
+
+$SHADER_MODULE_COMPUTE_BOUNDS$
+$SHADER_MODULE_COMPUTE_PHI$
+
 void main()
 {    
+    if(true){
+        vec3 central_difference = CalculateCentralDifference(direction, h2);
+        outputColor = vec4(central_difference,1);
+        return;
+    }
+
+    //OLD METHOD BELOW
+
     int x = int(gl_FragCoord[0]);
     int y = int(gl_FragCoord[1]);
 
@@ -63,7 +77,127 @@ void main()
     vec3 backward_value = texelFetch(texture_flow_map, pointer+extended_offset, 0).rgb;
 
     vec3 central_difference = (forward_value - backward_value) / h2;
-    outputColor = vec4(central_difference,1);
+    outputColor = vec4(central_difference,1);     
+}
+
+void ReadFlowmapValues(inout vec3 value_sample, inout vec3 value_forward, inout vec3 value_backward){
+    int x = int(gl_FragCoord[0]);
+    int y = int(gl_FragCoord[1]);
+
+    int forward_x = x;
+    int forward_y = y;
+    int forward_z = slice_index;
+
+    int backward_x = x;
+    int backward_y = y;
+    int backward_z = slice_index;
+
+    //identify the correct neighboring index. always +1 and -1 because flowmap is padded.
+    //direction X
+    if(direction == 0){
+        forward_x += 1;
+        backward_x -= 1;
+    }
+    //direction Y
+    else if(direction == 1){
+        forward_y += 1;
+        backward_y -= 1;
+    }
+    //direction Z
+    else{
+        forward_z += 1;  
+        backward_z -= 1;
+    }
+    //offset for backward data
+    if(!is_forward){
+        forward_z += dim_z+2; 
+        backward_z += dim_z+2;
+    }
+
+    ivec3 extended_offset = ivec3(1,1,1);
+
+    ivec3 pointer = ivec3(x,y,slice_index);
+    value_sample = texelFetch(texture_flow_map, pointer+extended_offset, 0).rgb;
+
+    pointer = ivec3(forward_x,forward_y,forward_z);
+    value_forward = texelFetch(texture_flow_map, pointer+extended_offset, 0).rgb;
+
+    pointer = ivec3(backward_x,backward_y,backward_z);
+    value_backward = texelFetch(texture_flow_map, pointer+extended_offset, 0).rgb;
+}
+
+vec3 CalculateCentralDifference(int direction, float h2){
+    //z_offset is used to access texture during interpolation
+    int z_offset = is_forward ? 0 : dim_z;
+    
+    //delta_x, delta_y, delta_z are used for calculating position_forward and position_backward
+    vec3 delta_x = vec3(1.0/(float(dim_x)-1.0), 0, 0);
+    vec3 delta_y = vec3(0, 1.0/(float(dim_y)-1.0), 0);
+    vec3 delta_z = vec3(0, 0, 1.0/(float(dim_z)-1.0));
+        
+    //Get sample/forward/backward positions in R3    
+    int x_index = int(gl_FragCoord[0]);
+    int y_index = int(gl_FragCoord[1]);
+    vec3 position_sample = vec3(float(x_index)/(float(dim_x)-1.0), float(y_index)/(float(dim_y)-1.0), float(slice_index)/(float(dim_z)-1.0));
+    vec3 direction_forward = (direction == 0) ? delta_x : (direction == 1) ? delta_y : delta_z;
+    vec3 direction_backward = -direction_forward;
+
+    //Transform forward or backward position if necessary (i.e. they are outside the FD)
+    vec3 position_forward = phi(position_sample, direction_forward);
+    vec3 position_backward = phi(position_sample, direction_backward);
+    
+    //Grab relative flow result from flow map
+    vec3 value_sample;
+    vec3 value_forward;
+    vec3 value_backward;
+    ReadFlowmapValues(value_sample, value_forward, value_backward);
+
+    //Add the starting positions and relative positions
+    value_sample += position_sample;
+    value_forward += position_sample + direction_forward;
+    value_backward += position_sample + direction_backward;
+
+    //Calculate central differences without using christoffel symbols
+    vec3 central_difference = (value_forward - value_backward) / h2;
+
+    //Correct the central differences using christoffel symbols
+    central_difference += CorrectionTerm(value_sample, position_sample);
+    
+    return central_difference;
+}
+
+vec3 CorrectionTerm(vec3 value_sample, vec3 position_sample){
+    vec3 correction = vec3(0,0,0);
+
+    //Rename for user convenience
+    float u1 = value_sample.x;
+    float u2 = value_sample.y;
+    float u3 = value_sample.z;
+    float x1 = position_sample.x;
+    float x2 = position_sample.y;
+    float x3 = position_sample.z;
+    
+    //add christoffel, ijk, i=function, j=direction, k=change
+    if(direction == 0)//j=0 (1 in mathematical notation)
+    {
+        correction.x += covariant_derivative_11k;
+        correction.y += covariant_derivative_21k;
+        correction.z += covariant_derivative_31k;
+    }
+    else if(direction == 1)//j=1 (2 in mathematical notation)
+    {
+        correction.x += covariant_derivative_12k;
+        correction.y += covariant_derivative_22k;
+        correction.z += covariant_derivative_32k;
+    }
+    else//j=2 (3 in mathematical notation)
+    {
+        correction.x += covariant_derivative_13k;
+        correction.y += covariant_derivative_23k;
+        correction.z += covariant_derivative_33k;
+    }
+
+    return correction;
 }
 
 `;
